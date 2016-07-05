cfd2572385dbacfd6170672ef338b6ceb3445c59
Richard Nienaber <rjnienaber@gmail.com>
2016-07-05 18:09:34 +0100

    Updated at hyperdev.com

---
+++ server.js
@@ -0,0 +1,42 @@
+// init project
+var express = require('express');
+var app = express();
+
+var ECT = require('ect');
+var ectRenderer = ECT({ ext : '.ect' });
+app.set('view engine', 'ect');
+app.engine('ect', ectRenderer.render);
+
+var request = require('request');
+var underscore = require('underscore');
+
+function processJokes(data) {
+  var filtered = underscore.filter(data.data.children, function (d) {
+    var length = d.data.title.length + d.data.selftext.length;
+    return length <= 140;
+  });
+  var topFive = underscore.sortBy(filtered, function (j) {
+    return j.data.score;
+  }).reverse().slice(0, 10);
+
+  return underscore.map(topFive, function (d) {
+    return underscore.pick(d.data, 'title', 'selftext', 'score', 'url');
+  });
+}
+
+app.get('/', function (req, res){
+  console.log("Status: OK");
+  res.send("Status: OK");
+});
+
+app.get('/risque', function (req, res){
+  request('https://www.reddit.com/r/jokes.json?limit=100', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.render('index', { jokes: processJokes(JSON.parse(body))});
+    }
+  });
+});
+
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
