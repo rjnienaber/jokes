2931b099e18093cd916cac0105809baca6d46e09
Richard Nienaber <rjnienaber@gmail.com>
2019-03-31 17:23:12 +0100

    add more joke sources, update code

--- server.js
+++ server.js
@@ -1,42 +1,45 @@
-// init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-var ECT = require('ect');
-var ectRenderer = ECT({ ext : '.ect' });
+const ECT = require('ect');
+const ectRenderer = ECT({ ext : '.ect' });
+
 app.set('view engine', 'ect');
 app.engine('ect', ectRenderer.render);
 
-var request = require('request');
-var underscore = require('underscore');
+const axios = require('axios');
+const lodash = require('lodash');
 
-function processJokes(data) {
-  var filtered = underscore.filter(data.data.children, function (d) {
-    var text = d.data.title + d.data.selftext;
+function processJokes(jokes) {
+  const filtered = lodash.filter(jokes, (d) => {
+    const text = d.data.title + d.data.selftext;
     return text.length <= 140 && !text.includes('discord');
   });
-  var topFive = underscore.sortBy(filtered, function (j) {
-    return j.data.score;
-  }).reverse().slice(0, 10);
 
-  return underscore.map(topFive, function (d) {
-    return underscore.pick(d.data, 'title', 'selftext', 'score', 'url');
-  });
+  const topFive = lodash.sortBy(filtered, (j) => j.data.score).reverse().slice(0, 10);
+  return lodash.map(topFive, (d) => lodash.pick(d.data, 'title', 'selftext', 'score', 'url'));
 }
 
-app.get('/', function (req, res){
+app.get('/', (req, res) => {
   console.log("Status: OK");
   res.send("Status: OK");
 });
 
-app.get('/risque', function (req, res){
-  request('https://www.reddit.com/r/jokes.json?limit=100', function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      res.render('index', { jokes: processJokes(JSON.parse(body))});
-    }
-  });
+app.get('/risque', (req, res) => {
+  const sources = [
+    axios.get('https://www.reddit.com/r/jokes.json?limit=100', {reponseType: 'json'}),
+    axios.get('https://www.reddit.com/r/dadjokes.json?limit=100', {reponseType: 'json'}),
+    axios.get('https://www.reddit.com/r/darkjokes.json?limit=100', {reponseType: 'json'})
+  ];
+
+  Promise.all(sources).then(responses => {
+    const allJokes = responses.reduce((acc, jokes) => acc.concat(jokes.data.data.children), []);
+    res.render('index', { jokes: processJokes(allJokes)});
+  }).catch((err) => {
+    res.status(500).send(err);
+  })
 });
 
-var listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
