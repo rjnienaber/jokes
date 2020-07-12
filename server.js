9bf708756e54d5e692ae4e388d61f490ddedf713
Richard Nienaber <rjnienaber@gmail.com>
2020-07-12 23:18:25 +0100

    take advantage of node 10

--- server.js
+++ server.js
@@ -11,19 +11,18 @@ const axios = require('axios');
 const lodash = require('lodash');
 
 function processJokes(jokes) {
-  const filtered = lodash.filter(jokes, (d) => {
+  const filtered = jokes.filter((d) => {
     const text = d.data.title + d.data.selftext;
     return text.length <= 140 && !text.includes('discord');
   });
 
   const bestJokes = lodash.sortBy(filtered, (j) => j.data.score).reverse().slice(0, 10);
-  return lodash.map(bestJokes, (d) => {
+  return bestJokes.map((d) => {
     const joke = lodash.pick(d.data, 'title', 'selftext', 'score', 'url');
     joke.encodedTitle = encodeURIComponent(joke.title);
     joke.encodedSelftext = encodeURIComponent(joke.selftext);
     return joke;
   });
-
 }
 
 app.get('/', (req, res) => {
@@ -31,19 +30,20 @@ app.get('/', (req, res) => {
   res.send("Status: OK");
 });
 
-app.get('/risque', (req, res) => {
+app.get('/risque', async (req, res) => {
   const sources = [
     axios.get('https://www.reddit.com/r/jokes.json?limit=100', {reponseType: 'json'}),
     axios.get('https://www.reddit.com/r/dadjokes.json?limit=100', {reponseType: 'json'}),
     axios.get('https://www.reddit.com/r/darkjokes.json?limit=100', {reponseType: 'json'})
   ];
 
-  Promise.all(sources).then(responses => {
+  try {
+    const responses = await Promise.all(sources);
     const allJokes = responses.reduce((acc, jokes) => acc.concat(jokes.data.data.children), []);
     res.render('index', { jokes: processJokes(allJokes)});
-  }).catch((err) => {
+  } catch (err) {
     res.status(500).send(err);
-  })
+  }
 });
 
 const listener = app.listen(process.env.PORT, () => {
